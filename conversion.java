2b0396abc0dad50b79f2cfa7c2199e803a0315af
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-08-21 11:16:14 +0530

    [preksha]conversion km to mile

---
+++ conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class conversion {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        double m=k * 0.621371;
+        System.out.println("kilometer to mile ="+m);
+    }
+}
