35b166d3f7175422575c124f125e7969727f643f
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-08-21 11:22:38 +0530

    [preksha]simple interest

---
+++ interest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class interest {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int r=sc.nextInt();
+        int t=sc.nextInt();
+        float s=(p*r*t)/100;
+        System.out.println("SIMPLE INTERSET="+s);
+}
+}
