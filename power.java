de9104095d63133aceb803c574dc4b416ff9d640
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-08-21 11:24:25 +0530

    [preksha]calculation of power

---
+++ power.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class power{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=(int)Math.pow(a,b);
+        System.out.println("POWER CALCULATION="+c);
+}
+}
