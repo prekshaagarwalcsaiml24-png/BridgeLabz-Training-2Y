f493b2a3be6fc531d59a6e8cfb0d4fe0691bf950
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-08-28 14:52:41 +0530

    [Preksha]Calculator

---
+++ Programming Elements/level1/BasicCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        float number1 = scanner.nextFloat();
+
+        System.out.print("Enter the second number: ");
+        float number2 = scanner.nextFloat();
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number2 != 0 ? number1 / number2 : 0;
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " +
+                           number1 + " and " + number2 + " is " +
+                           addition + ", " + subtraction + ", " + multiplication + ", and " + 
+                           (number2 != 0 ? division : "undefined (division by zero)") + ".");
+    }
+}
