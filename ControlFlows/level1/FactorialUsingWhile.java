98a6eaad3d8fbcc377d80832041b92c029150b5b
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-02 11:31:37 +0530

    Preksha

---
+++ ControlFlows/level1/FactorialUsingWhile.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FactorialUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid input. Please enter a non-negative integer.");
+            return;
+        }
+
+        long factorial = 1;
+        int i = 1;
+        while (i <= n) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + factorial);
+        sc.close();
+    }
+}
