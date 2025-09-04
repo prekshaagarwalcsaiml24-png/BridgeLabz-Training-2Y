6fcec235f101fbe4aead00e0fcffc32ee0f389a0
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-04 12:23:56 +0530

    Preksha

---
+++ ControlFlows/level2/FactorFinderWhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FactorFinderWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt(); 
+        if (number <= 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        int i = 1;
+        System.out.println("Factors of " + number + " are:");
+        while (i < number) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
