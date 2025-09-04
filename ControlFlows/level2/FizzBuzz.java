d024c38438c422f80c2f28028d594600f1ddaa97
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-04 12:29:00 +0530

    Preksha

---
+++ ControlFlows/level2/FizzBuzz.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt(); 
+        if (number <= 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        System.out.println("FizzBuzz results from 1 to " + number + ":");
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
