407d118f9099a82fe6b1754cf34eaa1af1f65c95
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-04 12:34:15 +0530

    Preksha

---
+++ ControlFlows/level2/FizzBuzzWhileLoop.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class FizzBuzzWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt(); 
+        if (number <= 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        int i = 1;
+        System.out.println("FizzBuzz results from 1 to " + number + ":");
+        while (i <= number) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++; 
+        }
+    } 
+} 
