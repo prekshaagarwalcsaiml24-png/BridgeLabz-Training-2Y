c6c8ef30fd2949d0831082b047739b433fc539f5
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-04 12:46:53 +0530

    Preksha

---
+++ ControlFlows/level2/GreatestFactorFinder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class GreatestFactorFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+        if (number <= 1) {
+            System.out.println("Please enter an integer greater than 1.");
+            return;
+        }
+        int greatestFactor = 1;
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+        System.out.println("The greatest factor of " + number + " (excluding itself) is: " + greatestFactor);
+    }
+}
