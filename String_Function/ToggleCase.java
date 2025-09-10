762ea0cef6f00810a33d1ae78a52c856cd1e0f15
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-10 15:40:04 +0530

    [Preksha]ToggleCase

---
+++ String_Function/ToggleCase.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ToggleCase {
+    public static String toggleCase(String text) {
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);
+            } else if (ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);
+            } else {
+                result += ch;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        sc.close();
+        String toggled = toggleCase(input);
+        System.out.println("Toggled case string: " + toggled);
+    }
+}
