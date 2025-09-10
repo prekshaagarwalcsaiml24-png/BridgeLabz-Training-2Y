e684730078e21d176c1c2eb3b3c925d159f60af3
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-10 11:46:36 +0530

    [Preksha]LongestWord

---
+++ String_Function/Level1/LongestWord.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class LongestWord {
+    public static String findLongestWord(String sentence) {
+        String[] words = sentence.split("\\s+");
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        return longest;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
+        sc.close();
+        String longestWord = findLongestWord(input);
+        System.out.println("The longest word is: " + longestWord);
+    }
+}
