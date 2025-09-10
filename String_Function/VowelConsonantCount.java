cca576f45f045993060d8068fb115d02ab5f5b88
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-10 15:41:52 +0530

    [Preksha]VowelConsonantCount

---
+++ String_Function/VowelConsonantCount.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class VowelConsonantCount {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        sc.close();
+        int vowels = 0;
+        int consonants = 0;
+        text = text.toLowerCase();
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Number of vowels: " + vowels);
+        System.out.println("Number of consonants: " + consonants);
+    }
+}
