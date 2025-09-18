bf590ec85f4f1ff72589ee2eaebdd31c3f8be668
prekshaagarwalcsaiml24-p <preksha.agarwal_cs.aiml24@gla.ac.in>
2025-09-18 12:00:31 +0530

    [Preksha]

---
+++ Inheritance/Assisted Problems/TransportSystemDemo.java
@@ -0,0 +1,73 @@
+package AssistedProblems;
+class Vehicle {
+    protected int maxSpeed;
+    protected String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+class Car extends Vehicle {
+    private int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+class Truck extends Vehicle {
+    private double loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Subclass: Motorcycle
+class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+public class TransportSystemDemo {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+            new Car(180, "Petrol", 5),
+            new Truck(120, "Diesel", 15.5),
+            new Motorcycle(150, "Petrol", false)
+        };
+
+        for (Vehicle v : vehicles) {
+            System.out.println("\n--- Vehicle Info ---");
+            v.displayInfo();  // Polymorphic call
+        }
+    }
+}
